6514a4e3a9f3c43f123a53577e0e134a1f0a00ba
FryderykSzopen <creeperscrown@gmail.com>
2017-06-30 17:00:43 +0200

    Add files via upload

---
+++ morfoloskoRasclanjivanje.py
@@ -0,0 +1,19 @@
+w1 = "pevač"
+w2 = "beogradski"
+w3 = "spavanje"
+
+a=w1[-6:-2]
+aa=w1[3:5]
+b=w2[0:7]
+bb=w2[7:11]
+c=w3[0:-5]
+cc=w3[-3:]
+c_c=w3[4]
+
+print("Osnova: ",a)
+print("Sufiks: ",aa)
+print("Osnova: ",b)
+print("Sufiks: ",bb)
+print("Osnova: ",c)
+print("Tematski vokal: ",c_c)
+print("Sufiks: ",cc)
