7edf85d97d3a939e402b6b2223a7f06d27de65d0
FryderykSzopen <creeperscrown@gmail.com>
2017-06-30 16:25:52 +0200

    Add files via upload

---
+++ rasclanjivanje.py
@@ -0,0 +1,8 @@
+a="Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je: \"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+a=a.lower()
+b=a.split(" ")
+print(b)
+
+c="Dokle cu vise biti niska!"
+x=list(c)
+print(x)
