6b27902043c59d14f93c7688f516c4c22198f477
FryderykSzopen <creeperscrown@gmail.com>
2017-07-02 02:01:45 +0200

    Add files via upload

---
+++ RecnikSinonimaIAntonima.py
@@ -0,0 +1,8 @@
+recnik={"happy": ["sinonimi": ["delighted","overjoyed","gladdened"]],["antonimi":["miserable","unhappy","sorrowful"]], "unearth":["sinonimi"["excavate","uncover","grub"]],"antonimi":["bury","cover","conceal"]], "kill":["sinonimi":["murder", "terminate", "dispatch"]],"antonimi":["revive", "reinvigorate", "restore"]]}
+print(recnik["happy"]["sinonimi"])
+print(recnik["happy"]["antonimi"])
+print(recnik["unearth"]["sinonimi"])
+print(recnik["unearth"]["antonimi"])
+print(recnik["kill"]["sinonimi"])
+print(recnik["kill"]["antonimi"])
+#Ne radi, lol, ne znam kako da resim problem sa sintaksom.
